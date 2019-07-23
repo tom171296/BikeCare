ed97f17e2c514b9ef1257b5e3003b08796ec7074
Tom van den Berg <t.vandenberg@infosupport.com>
2019-07-23 20:56:17 +0200

    removed values controller

---
+++ Src/BikeManagementAPI/BikeManagementAPI/Controllers/BikeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BikeManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BikeController
+    {
+    }
+}
