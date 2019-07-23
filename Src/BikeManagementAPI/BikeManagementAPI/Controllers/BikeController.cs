70a68e1600756266a575288a34627b50ae29ad20
Tom van den Berg <t.vandenberg@infosupport.com>
2019-07-23 21:09:39 +0200

    added bike controller api

--- Src/BikeManagementAPI/BikeManagementAPI/Controllers/BikeController.cs
+++ Src/BikeManagementAPI/BikeManagementAPI/Controllers/BikeController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BikeManagementAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BikeController
+    public class BikeController : Controller
     {
+        [HttpGet]
+        public async Task<IActionResult> GetBikes()
+        {
+            return Ok();
+        }
     }
 }
