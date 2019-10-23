2e68d10fdaf3fd3f2f8e99c21e616aa5873a56af
Tom van den Berg <t.vandenberg@infosupport.com>
2019-10-23 15:39:01 +0200

    added first steps

---
+++ Src/BikeManagementAPI/BikeManagementAPITests/StepDefinitions/When/WhenBike.cs
@@ -0,0 +1,22 @@
+﻿using BikeManagementAPITests.Support.Context;
+using TechTalk.SpecFlow;
+
+namespace BikeManagementAPITests.StepDefinitions.When
+{
+    [Binding]
+    public class WhenBike
+    {
+        private readonly UserContext _userContext;
+
+        public WhenBike(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        [When(@"I register a new bike")]
+        public void WhenIRegisterANewBike()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
